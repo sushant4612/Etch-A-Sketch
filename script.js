add57afd86a6da16a3f32fa219a4d7884185752b
“sushant4612” <sushantsp14@gmail.com>
2023-01-03 15:37:03 +0530

    Add hover effect

--- script.js
+++
@@ -1,9 +0,0 @@
-const hover = document.getElementsByClassName(".elements");
-console.log(hover);
-hover.addEventListener("click", function backround() {
-  hover.style.backroundColor = "red";
-});
-
-function changeColor() {
-  console.log(hover);
-}
